29c00922e5cb5732691de1ccd1ba1099840180df
wzbillings <39701545+wzbillings@users.noreply.github.com>
2021-09-15 20:08:21 -0400

    exploratory plots

--- code/analysis_code/analysisscript.R
+++ code/analysis_code/analysisscript.R
@@ -5,9 +5,9 @@
 #and saves the results to the results folder
 
 #load needed packages. make sure they are installed.
-library(ggplot2) #for plotting
-library(broom) #for cleaning up output from lm()
+library(tidyverse) #for plotting
 library(here) #for data loading/saving
+library(usmap)
 
 #path to data
 #note the use of the here() package and not absolute paths
@@ -19,51 +19,44 @@ mydata <- readRDS(data_location)
 ######################################
 #Data exploration/description
 ######################################
-#I'm using basic R commands here.
-#Lots of good packages exist to do more.
-#For instance check out the tableone or skimr packages
 
-#summarize data 
-mysummary = summary(mydata)
+# Plot of covid 19 deaths over time stratified by health condition and age group
+p1 <- mydata |>
+  group_by(age.group, health.condition, start.date) |>
+  summarize(y = mean(covid.19.deaths, na.rm = TRUE)) |>
+  ggplot(aes(x = start.date, y = y, col = age.group)) +
+  geom_line() +
+  facet_wrap(~health.condition, scales = "free_y") +
+  theme_bw() +
+  labs(x = "date", y = "mean COVID-19 deaths",
+       title = "COVID-19 deaths in Georgia over time stratified by age group and comorbidities")
 
-#look at summary
-print(mysummary)
+ggsave(p1, filename = here::here("results", "p1.png"), height = 8.5, width = 11)
 
-#do the same, but with a bit of trickery to get things into the 
-#shape of a data frame (for easier saving/showing in manuscript)
-summary_df = data.frame(do.call(cbind, lapply(mydata, summary)))
+# mean deaths in georgia by month
+p2 <- mydata %>%
+  filter(state == "Georgia", health.condition == "COVID-19") %>%
+  group_by(month) %>%
+  summarize(mean_se(covid.19.deaths)) %>%
+  ungroup() %>%
+  ggplot(aes(x = month, y = y, ymin = ymin, ymax = ymax, group = 1)) +
+  geom_ribbon(alpha = 0.5, fill = "cadetblue") +
+  geom_line(size = 1, lineend = "round", linejoin = "round") +
+  scale_x_discrete(labels = month.abb) +
+  theme_bw() +
+  labs(x = NULL, y = "mean COVID-19 deaths (+/- 1 SE)",
+       title = "COVID-19 deaths by month with no comorbidities for Georgia")
 
-#save data frame table to file for later use in manuscript
-summarytable_file = here("results", "summarytable.rds")
-saveRDS(summary_df, file = summarytable_file)
+ggsave(p2, filename = here::here("results", "p2.png"), height = 8.5, width = 11)
 
+p3 <- mydata %>%
+  filter(health.condition == "COVID-19") %>%
+  group_by(state, age.group) %>%
+  summarize(deaths = max(covid.19.deaths)) %>%
+  plot_usmap(regions = "states", values = "deaths", data = .) +
+  facet_wrap(~age.group, nrow = 2) +
+  scale_fill_viridis_c(name = "deaths") +
+  theme(legend.position = "bottom", legend.justification = "center") +
+  ggtitle("Overall maximum reported COVID-19 deaths for each state by age group")
 
-#make a scatterplot of data
-#we also add a linear regression line to it
-p1 <- mydata %>% ggplot(aes(x=Height, y=Weight)) + geom_point() + geom_smooth(method='lm')
-
-#look at figure
-plot(p1)
-
-#save figure
-figure_file = here("results","resultfigure.png")
-ggsave(filename = figure_file, plot=p1) 
-
-######################################
-#Data fitting/statistical analysis
-######################################
-
-# fit linear model
-lmfit <- lm(Weight ~ Height, mydata)  
-
-# place results from fit into a data frame with the tidy function
-lmtable <- broom::tidy(lmfit)
-
-#look at fit results
-print(lmtable)
-
-# save fit results table  
-table_file = here("results", "resulttable.rds")
-saveRDS(lmtable, file = table_file)
-
-  
+ggsave(p3, filename = here::here("results", "p3.png"), height = 8.5, width = 11)
